9fb8a5e8671913046899bf7840567e78fa0f94ed
Stef Coenen <stef@thebrownfox.be>
2023-09-08 11:35:42 +0200

    Expanded LoginBox with setting youauth meta tag;

--- src/components/Auth/LoginBox/LoginBox.tsx
+++ src/components/Auth/LoginBox/LoginBox.tsx
@@ -4,6 +4,7 @@ import { IS_DARK_CLASSNAME } from '../../../hooks/useDarkMode';
 import LoadingBlock from '../../ui/Layout/Loaders/LoadingBlock/LoadingBlock';
 import { stringifyToQueryParams } from '@youfoundation/js-lib/helpers';
 import { YouAuthorizationParams } from '@youfoundation/js-lib/auth';
+import { Helmet } from 'react-helmet-async';
 
 export const LoginBox = () => {
   const [authParams, setAuthParams] = useState<YouAuthorizationParams>();
@@ -23,13 +24,20 @@ export const LoginBox = () => {
     );
 
   return (
-    <iframe
-      src={`${
-        import.meta.env.VITE_CENTRAL_LOGIN_URL
-      }?isDarkMode=${document.documentElement.classList.contains(IS_DARK_CLASSNAME)}${
-        authParams ? `&${stringifyToQueryParams(authParams as any)}` : ''
-      }`}
-      className="h-[16rem] w-full"
-    ></iframe>
+    <>
+      {authParams ? (
+        <Helmet>
+          <meta name="youauth" content={stringifyToQueryParams(authParams as any)} />
+        </Helmet>
+      ) : null}
+      <iframe
+        src={`${
+          import.meta.env.VITE_CENTRAL_LOGIN_URL
+        }?isDarkMode=${document.documentElement.classList.contains(IS_DARK_CLASSNAME)}${
+          authParams ? `&${stringifyToQueryParams(authParams as any)}` : ''
+        }`}
+        className="h-[16rem] w-full"
+      ></iframe>
+    </>
   );
 };
