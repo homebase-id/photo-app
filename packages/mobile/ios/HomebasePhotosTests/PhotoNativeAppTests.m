887efae2be181a58b4a1f348af50edb81e645b20
Stef Coenen <stef@thebrownfox.be>
2023-10-12 17:39:23 +0200

    Renamed native project to HomebasePhotos;

--- packages/mobile/ios/HomebasePhotosTests/PhotoNativeAppTests.m
+++ packages/mobile/ios/HomebasePhotosTests/PhotoNativeAppTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface PhotoNativeAppTests : XCTestCase
+@interface HomebasePhotosTests : XCTestCase
 
 @end
 
-@implementation PhotoNativeAppTests
+@implementation HomebasePhotosTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
