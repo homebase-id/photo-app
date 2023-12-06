6e78192ab9e678f7d26674b3aeecc732a0c56375
Stef Coenen <stef@thebrownfox.be>
2023-12-06 09:56:51 +0100

    Added missing create button on album dialog;

--- packages/mobile/src/components/PhotoAlbum/NewAlbumDialog.tsx
+++ packages/mobile/src/components/PhotoAlbum/NewAlbumDialog.tsx
@@ -1,27 +1,17 @@
 import React, { useState } from 'react';
 import { Input, Modal, TextArea } from 'native-base';
-import { View } from 'react-native';
+import { Button, View } from 'react-native';
 import { Text } from '../ui/Text/Text';
 import { Colors } from '../../app/Colors';
 import { getNewId } from '@youfoundation/js-lib/helpers';
 import { useAlbum } from '../../hooks/photoLibrary/useAlbum';
 import { useDarkMode } from '../../hooks/useDarkMode';
 
-const NewAlbumDialog = ({
-  isOpen,
-  onClose,
-}: {
-  isOpen: boolean;
-  onClose: () => void;
-}) => {
+const NewAlbumDialog = ({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) => {
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
 
-  const {
-    mutateAsync: saveAlbum,
-    status: saveStatus,
-    error: saveError,
-  } = useAlbum().save;
+  const { mutateAsync: saveAlbum, status: saveStatus, error: saveError } = useAlbum().save;
 
   const doSaveAlbum = async () => {
     const newTag = getNewId();
@@ -42,26 +32,29 @@ const NewAlbumDialog = ({
           width: '100%',
           maxHeight: '60%',
           backgroundColor: isDarkMode ? Colors.gray[900] : Colors.slate[50],
-        }}>
+        }}
+      >
         <Modal.CloseButton />
         <Modal.Header
           style={{
             borderBottomWidth: 0,
             backgroundColor: isDarkMode ? Colors.gray[900] : Colors.slate[50],
-          }}>
+          }}
+        >
           New Album
         </Modal.Header>
         <Modal.Body
           style={{
             backgroundColor: isDarkMode ? Colors.gray[900] : Colors.slate[50],
-          }}>
+          }}
+        >
           {/* Name */}
           <View style={{ marginBottom: 30, width: '100%' }}>
             <Text>Name</Text>
             <Input
               w="100%"
               placeholder="Add a name"
-              onChangeText={val => setName(val)}
+              onChangeText={(val) => setName(val)}
               style={{
                 fontSize: 16,
               }}
@@ -75,13 +68,14 @@ const NewAlbumDialog = ({
               w="100%"
               autoCompleteType={'off'}
               placeholder="Add a description"
-              onChangeText={val => setDescription(val)}
+              onChangeText={(val) => setDescription(val)}
               style={{
                 fontSize: 16,
               }}
               onSubmitEditing={name ? doSaveAlbum : undefined}
             />
           </View>
+          <Button title="Create" onPress={doSaveAlbum} />
         </Modal.Body>
       </Modal.Content>
     </Modal>
