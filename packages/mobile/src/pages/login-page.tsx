d96057910675dbcd181ad026c40c6889affa1325
Stef Coenen <stef@thebrownfox.be>
2023-12-11 18:15:32 +0100

    Improved dark mode colors on login;

--- packages/mobile/src/pages/login-page.tsx
+++ packages/mobile/src/pages/login-page.tsx
@@ -22,6 +22,7 @@ import { InAppBrowser } from 'react-native-inappbrowser-reborn';
 
 import logo from './homebase-photos.png';
 import { YouAuthorizationParams } from '@youfoundation/js-lib/auth';
+import { useDarkMode } from '../hooks/useDarkMode';
 
 type LoginProps = NativeStackScreenProps<AuthStackParamList, 'Login'>;
 
@@ -145,6 +146,7 @@ const useParams = () => {
 const LoginComponent = () => {
   // LoginPage is the only page where you can be when unauthenticated; So only page where we listen for a finalize return link
   const finalizeState = useFinalize();
+  const { isDarkMode } = useDarkMode();
 
   const [invalid, setInvalid] = useState<boolean>(false);
   const [odinId, setOdinId] = useState<string>('');
@@ -192,9 +194,10 @@ const LoginComponent = () => {
           height: 40,
           marginVertical: 12,
           borderWidth: 1,
-          borderColor: Colors.slate[300],
+          borderColor: isDarkMode ? Colors.slate[700] : Colors.slate[300],
           borderRadius: 4,
           padding: 10,
+          color: isDarkMode ? Colors.white : Colors.black,
         }}
         onChangeText={setOdinId}
         autoCapitalize="none"
