2a78ec342ec24a35a96c7afd1ed5f440ea6d6b0f
Stef Coenen <stef@thebrownfox.be>
2024-01-31 18:11:32 +0100

    Add support for showing the cameraRoll;

---
+++ packages/mobile/src/hooks/cameraRoll/useCameraRoll.ts
@@ -0,0 +1,25 @@
+import { CameraRoll } from '@react-native-camera-roll/camera-roll';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+const BATCH_SIZE = 50;
+export const useCameraRoll = (fromTime?: number) => {
+  const getPhotos = async (fromTime: number | undefined, cursor: string | undefined) => {
+    return await CameraRoll.getPhotos({
+      first: BATCH_SIZE,
+      fromTime: fromTime,
+      assetType: 'All',
+      after: cursor,
+    });
+  };
+
+  return {
+    fetch: useInfiniteQuery({
+      queryKey: ['cameraRoll', fromTime || 'now'],
+      initialPageParam: undefined as string | undefined,
+      queryFn: ({ pageParam }) => getPhotos(fromTime, pageParam),
+      getNextPageParam: (lastPage) => lastPage.page_info.end_cursor,
+      gcTime: 0,
+      staleTime: 0,
+    }),
+  };
+};
--- packages/mobile/src/pages/sync-details-page.tsx
+++ packages/mobile/src/pages/sync-details-page.tsx
@@ -1,6 +1,6 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React, { useEffect } from 'react';
-import { Alert, Button, View } from 'react-native';
+import React, { ReactNode, useEffect, useMemo } from 'react';
+import { Alert, Button, Dimensions, FlatList, Image, Platform, View } from 'react-native';
 import { Text } from '../components/ui/Text/Text';
 
 import { SettingsStackParamList } from '../app/App';
@@ -9,9 +9,11 @@ import { Container } from '../components/ui/Container/Container';
 import { useKeyValueStorage } from '../hooks/auth/useEncryptedStorage';
 import { useSyncFromCameraRoll } from '../hooks/cameraRoll/useSyncFromCameraRoll';
 import { hasAndroidPermission } from '../hooks/cameraRoll/permissionHelper';
+import { useCameraRoll } from '../hooks/cameraRoll/useCameraRoll';
 
 type SettingsProps = NativeStackScreenProps<SettingsStackParamList, 'Profile'>;
 
+const PREFFERED_IMAGE_SIZE = 90;
 const dateFormat: Intl.DateTimeFormatOptions = {
   month: 'short',
   day: 'numeric',
@@ -22,11 +24,10 @@ const dateFormat: Intl.DateTimeFormatOptions = {
 
 const SyncDetailsPage = (_props: SettingsProps) => {
   const [syncNowState, setSyncNowState] = React.useState<'idle' | 'pending' | 'finished'>('idle');
-  const [pendingCount, setPendingCount] = React.useState<number | null>(null);
 
   const { setSyncFromCameraRoll, syncFromCameraRoll, lastCameraRollSyncTime } =
     useKeyValueStorage();
-  const { forceSync, getWhatsPending } = useSyncFromCameraRoll(false);
+  const { forceSync } = useSyncFromCameraRoll(false);
 
   const doSyncNow = async () => {
     setSyncNowState('pending');
@@ -36,100 +37,194 @@ const SyncDetailsPage = (_props: SettingsProps) => {
   const lastSyncAsInt = lastCameraRollSyncTime ? parseInt(lastCameraRollSyncTime || '') : null;
 
   useEffect(() => {
-    (async () => setPendingCount(await getWhatsPending()))();
-  }, [getWhatsPending]);
-
-  useEffect(() => {
     (async () => {
-      hasAndroidPermission();
+      if (Platform.OS === 'android') hasAndroidPermission();
     })();
   }, []);
 
   return (
     <SafeAreaView>
-      <Container>
-        <View style={{ display: 'flex', flexDirection: 'column' }}>
-          {syncFromCameraRoll ? (
-            <>
-              <View
-                style={{
-                  marginLeft: 16,
-                  marginTop: 16,
-                  opacity: 0.5,
-                  flexDirection: 'column',
-                  gap: 4,
-                }}
-              >
-                <Text>
-                  {syncNowState === 'idle' ? (
-                    <>
-                      {lastSyncAsInt ? (
-                        <>
-                          Last sync: {new Date(lastSyncAsInt).toLocaleString(undefined, dateFormat)}
-                        </>
-                      ) : (
-                        'Last sync: did not sync yet'
-                      )}
-                    </>
-                  ) : (
-                    <>
-                      {syncNowState === 'pending'
-                        ? 'Syncing...'
-                        : syncNowState === 'finished'
-                        ? 'Last sync: Just now'
-                        : null}
-                    </>
-                  )}
-                </Text>
-                <Text>
-                  {pendingCount === 0
-                    ? 'All your photos are safe and secure on your identity'
-                    : `Pending: ${pendingCount}`}
-                </Text>
-              </View>
+      <GalleryView>
+        <Container>
+          <View style={{ display: 'flex', flexDirection: 'column' }}>
+            {syncFromCameraRoll ? (
+              <>
+                <View
+                  style={{
+                    marginLeft: 8,
+                    marginTop: 8,
+                    opacity: 0.5,
+                    flexDirection: 'column',
+                    gap: 4,
+                  }}
+                >
+                  <Text>
+                    {syncNowState === 'idle' ? (
+                      <>
+                        {lastSyncAsInt ? (
+                          <>
+                            Last sync:{' '}
+                            {new Date(lastSyncAsInt).toLocaleString(undefined, dateFormat)}
+                          </>
+                        ) : (
+                          'Last sync: did not sync yet'
+                        )}
+                      </>
+                    ) : (
+                      <>
+                        {syncNowState === 'pending'
+                          ? 'Syncing...'
+                          : syncNowState === 'finished'
+                          ? 'Last sync: Just now'
+                          : null}
+                      </>
+                    )}
+                  </Text>
+                </View>
 
-              <View
-                style={{
-                  marginTop: 'auto',
-                  paddingTop: 50,
-                  flexDirection: 'row-reverse',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <Button title="Sync now" onPress={doSyncNow} />
-                <Button
-                  title="Disable"
-                  onPress={() =>
-                    Alert.alert('Disable sync?', 'Your existing photos will not be removed', [
-                      {
-                        text: 'Disable',
-                        onPress: () => setSyncFromCameraRoll(false),
-                      },
-                      {
-                        text: 'Cancel',
-                        style: 'cancel',
-                      },
-                    ])
-                  }
-                />
-              </View>
-            </>
-          ) : (
-            <>
-              <Text
-                style={{
-                  marginTop: 16,
-                }}
-              >
-                Back-up all media from now forward into your identity. Your photos will remain
-                secured and only acessible by you.
-              </Text>
-              <Button title="Enable" onPress={() => setSyncFromCameraRoll(true)} />
-            </>
-          )}
-        </View>
-      </Container>
+                <View
+                  style={{
+                    marginTop: 'auto',
+                    paddingTop: 8,
+                    flexDirection: 'row-reverse',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Button title="Sync now" onPress={doSyncNow} />
+                  <Button
+                    title="Disable"
+                    onPress={() =>
+                      Alert.alert('Disable sync?', 'Your existing photos will not be removed', [
+                        {
+                          text: 'Disable',
+                          onPress: () => setSyncFromCameraRoll(false),
+                        },
+                        {
+                          text: 'Cancel',
+                          style: 'cancel',
+                        },
+                      ])
+                    }
+                  />
+                </View>
+              </>
+            ) : (
+              <>
+                <Text
+                  style={{
+                    marginTop: 16,
+                  }}
+                >
+                  Back-up all media from now forward into your identity. Your photos will remain
+                  secured and only acessible by you.
+                </Text>
+                <Button title="Enable" onPress={() => setSyncFromCameraRoll(true)} />
+              </>
+            )}
+          </View>
+        </Container>
+      </GalleryView>
     </SafeAreaView>
+  );
+};
+
+const GalleryView = ({ children }: { children: ReactNode }) => {
+  const { lastCameraRollSyncTime } = useKeyValueStorage();
+  const lastCameraRollSyncTimeAsInt = lastCameraRollSyncTime
+    ? parseInt(lastCameraRollSyncTime)
+    : new Date().getTime();
+
+  const lastWeek = new Date().getTime() - 1000 * 60 * 60 * 24 * 7;
+  const fromTime = Math.min(lastCameraRollSyncTimeAsInt, lastWeek);
+
+  const {
+    data: cameraRoll,
+    fetchNextPage,
+    hasNextPage,
+    isFetchingNextPage,
+  } = useCameraRoll(fromTime).fetch;
+
+  const flatPhotos = useMemo(
+    () => cameraRoll?.pages.flatMap((page) => page.edges).map((edge) => edge.node) || [],
+    [cameraRoll]
+  );
+
+  const windowSize = Dimensions.get('window');
+  const numColums = Math.round(windowSize.width / PREFFERED_IMAGE_SIZE);
+  const size = Math.round(windowSize.width / numColums);
+
+  const anyPending = flatPhotos.length > 0;
+
+  return (
+    <View
+      style={{
+        margin: -1,
+      }}
+    >
+      <FlatList
+        data={flatPhotos}
+        key={numColums}
+        style={{
+          minHeight: '100%',
+        }}
+        ListHeaderComponent={
+          <>
+            {children}
+            {anyPending ? (
+              <Text
+                style={{ paddingHorizontal: Platform.OS === 'ios' ? 15 : 5, paddingBottom: 5 }}
+              >{`${flatPhotos.length} pending upload`}</Text>
+            ) : null}
+          </>
+        }
+        renderItem={(item) => (
+          <View
+            key={item.item.id}
+            style={{
+              width: size,
+              height: size,
+              padding: 1,
+              position: 'relative',
+            }}
+          >
+            <Image
+              key={item.item.id}
+              style={{
+                width: size - 2,
+                height: size - 2,
+              }}
+              source={{ uri: item.item.image.uri }}
+            />
+          </View>
+        )}
+        ListEmptyComponent={
+          <>
+            <Text
+              style={{
+                paddingHorizontal: 5,
+                paddingBottom: 5,
+                textAlign: 'center',
+                opacity: 0.5,
+              }}
+            >
+              No recent media
+            </Text>
+            <Text
+              style={{
+                paddingHorizontal: 5,
+                paddingBottom: 5,
+                textAlign: 'center',
+                opacity: 0.5,
+              }}
+            >
+              During alpha, we only support synchronizing recent media
+            </Text>
+          </>
+        }
+        numColumns={numColums}
+        onEndReached={() => hasNextPage && !isFetchingNextPage && fetchNextPage()}
+      />
+    </View>
   );
 };
 
