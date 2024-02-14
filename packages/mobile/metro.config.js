344d9af3b26a187fd199e066fee4e5e3b47f6f04
Stef Coenen <stef@thebrownfox.be>
2024-02-14 15:16:03 +0100

    More perf more betterer (#33)
    * Extra optimisations on the lib and preview; Extra logging for debugging;
    
    * Upgraded react-native to 0.73.4;
    
    * Cleaned out logs;

--- packages/mobile/metro.config.js
+++ packages/mobile/metro.config.js
@@ -1,10 +1,26 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const path = require('path');
-
+// console.log('metro.config.js', path.resolve(__dirname, '../../node_modules'));
 const config = {
-  projectRoot: path.resolve(__dirname, '../../'),
+  // projectRoot: path.resolve(__dirname, '../../'),
+  projectRoot: __dirname,
+  watchFolders: [path.resolve(__dirname, '../../'), path.resolve(__dirname, '../../packages')],
+  // extraNodeModules: [
+  //   path.resolve(__dirname, '../../web/node_modules'),
+  //   path.resolve(__dirname, '../../node_modules'),
+  // ],
   resolver: {
+    extraNodeModules: {
+      'photo-app-common': path.resolve(__dirname, '../../packages/common'),
+      '@react-native-community/netinfo': path.resolve(
+        __dirname,
+        '../../node_modules/@react-native-community/netinfo'
+      ),
+    },
     resetCache: true,
+    // unstable_enableSymlinks: true,
+    // unstable_enablePackageExports: true,
+    // enableSymlinks: true,
   },
 };
 
