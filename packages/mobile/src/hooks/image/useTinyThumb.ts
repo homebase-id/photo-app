29f827c7c288e945eb1e03d292d8a26a7d26f867
Stef Coenen <stef@thebrownfox.be>
2024-05-22 17:15:51 +0200

    Updated OdinImage;

--- packages/mobile/src/hooks/image/useTinyThumb.ts
+++ packages/mobile/src/hooks/image/useTinyThumb.ts
@@ -1,32 +1,49 @@
 import { useQuery } from '@tanstack/react-query';
 
-import { TargetDrive, DEFAULT_PAYLOAD_KEY } from '@youfoundation/js-lib/core';
+import { TargetDrive } from '@youfoundation/js-lib/core';
 import { getDecryptedThumbnailMeta } from '@youfoundation/js-lib/media';
 import { useDotYouClientContext } from 'photo-app-common';
 
-const useTinyThumb = (odinId?: string, imageFileId?: string, imageDrive?: TargetDrive) => {
+const useTinyThumb = ({
+  odinId,
+  imageFileId,
+  imageFileKey,
+  imageDrive,
+}: {
+  odinId?: string;
+  imageFileId?: string;
+  imageFileKey?: string;
+  imageDrive?: TargetDrive;
+}) => {
   const dotYouClient = useDotYouClientContext();
-  const fetchImageData = async (odinId: string, imageFileId?: string, imageDrive?: TargetDrive) => {
-    if (imageFileId === undefined || imageFileId === '' || !imageDrive) return;
-
+  const fetchImageData = async (
+    odinId: string,
+    imageFileId?: string,
+    imageFileKey?: string,
+    imageDrive?: TargetDrive
+  ) => {
+    if (imageFileId === undefined || imageFileId === '' || !imageDrive || !imageFileKey) {
+      return null;
+    }
     return (
       (await getDecryptedThumbnailMeta(
         dotYouClient,
         imageDrive,
         imageFileId,
-        DEFAULT_PAYLOAD_KEY,
+        imageFileKey,
         'Standard'
       )) || null
     );
   };
 
   return useQuery({
-    queryKey: ['tinyThumb', odinId, imageFileId, imageDrive?.alias],
-    queryFn: () => fetchImageData(odinId as string, imageFileId, imageDrive),
+    queryKey: ['tinyThumb', odinId, imageFileId, imageDrive?.alias || ''],
+    queryFn: () =>
+      fetchImageData(odinId as string, imageFileId, imageFileKey as string, imageDrive),
     refetchOnMount: true,
     refetchOnWindowFocus: false,
     staleTime: 1000 * 60 * 60 * 1, // 1h
-    enabled: !!imageFileId && imageFileId !== '' && !!odinId,
+    enabled: !!imageFileId && imageFileId !== '' && !!odinId && !!imageFileKey,
   });
 };
 
