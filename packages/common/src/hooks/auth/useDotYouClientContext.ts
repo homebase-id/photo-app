047b37ff72e9db118c2aecae6e6377f162b70651
Stef Coenen <stef@thebrownfox.be>
2025-02-04 15:23:33 +0100

    Extra dotyouclient fix; (#84)

--- packages/common/src/hooks/auth/useDotYouClientContext.ts
+++ packages/common/src/hooks/auth/useDotYouClientContext.ts
@@ -1,11 +1,15 @@
-import { DotYouClient } from '@homebase-id/js-lib/core';
+import { ApiType, DotYouClient } from '@homebase-id/js-lib/core';
 import { useContext, createContext } from 'react';
 
 export const DotYouClientContext = createContext<DotYouClient | null>(null);
 
 export const useDotYouClientContext = () => {
   const dotYouClient = useContext(DotYouClientContext);
-  if (!dotYouClient) throw new Error('DotYouClientContext not found');
-
-  return dotYouClient;
+  return (
+    dotYouClient ||
+    new DotYouClient({
+      api: ApiType.Guest,
+      hostIdentity: '',
+    })
+  );
 };
